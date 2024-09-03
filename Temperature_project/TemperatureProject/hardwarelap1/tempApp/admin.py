e947d5346de1f066c5017a42d649a5c42602a4d6
Linda M. <128227979+linda-tech22@users.noreply.github.com>
2024-09-03 11:51:18 +0200

    Delete Temperature_project/TemperatureProject/hardwarelap1 directory

--- Temperature_project/TemperatureProject/hardwarelap1/tempApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import TemperatureReading
-
-# Register your models here.
-
-admin.site.register(TemperatureReading)
--- Temperature_project/TemperatureProject/hardwarelap1/tempApp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class TemperatureReading(models.Model):
-    id = models.IntegerField(primary_key=True)
-    temperature = models.FloatField()
-    timestamp = models.DateTimeField(auto_now_add=True)
